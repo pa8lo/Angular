8155100bdfb13f90d6416b86c4ee970b70ab3bdd
Rozek <pablo.rozek@externos-ar.cencosud.com>
2019-09-04 18:55:15 -0300

    second week

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -6,19 +6,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule} from '@angular/flex-layout';
-
+import { MatListModule} from '@angular/material/list'
 import 'hammerjs';
-
+import { MenuComponent } from './menu/menu.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishDetailComponent } from './menu/dishdetails/dishdetails.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
